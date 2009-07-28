73c42b16664cf6ac1dfd62aa1a5fdac60a72602b
jhochenbaum <jhochenbaum@localhost>
2009-07-29 00:27:45 +1200

    Began adding node fades, and put Mandala into offscreen buffer for rendering.

--- src/MandalaViewController.java
+++ src/MandalaViewController.java
@@ -1,5 +1,8 @@
 import processing.core.PApplet;
 import processing.core.PConstants;
+import processing.core.PFont;
+import processing.core.PGraphics;
+import processing.core.PImage;
 
 // ========================================================================= // 
 // AH-Opera | Interactive 2D Mandala Wheel
@@ -11,24 +14,54 @@ import processing.core.PConstants;
 public class MandalaViewController {
 
 	float rot;
-	float addfill = 100;
 	float addfillb = 10;
 	float addfillc = 255;
 	float pulse;
 	float pulsate;
 	float pulser;
 	float angle;
-	int button;
 	float x;
 	float y;
+	float xCenter;
+	float yCenter;
+	float circleRadius;
+	float circleStroke;
+	float nodeRadius;
+	float nodeStroke;
+	float numberOfNodes = 12;
+	int button;
 	
 
-	PApplet parent; // The parent PApplet that we will render ourselves onto
+	PImage image;
+	PGraphics pg;
+	PFont mandalaFont;
+	MandalaHover mandalaHover;
 	MandalaNode[] mandalaNodeList;
+
 	
-	MandalaViewController(PApplet p, MandalaNode[] x) {
+	PApplet parent; // The parent PApplet that we will render ourselves onto
+
+	
+	
+	MandalaViewController(PApplet p, PGraphics pgraphics ) {
 		parent = p;
-		mandalaNodeList = x;
+		pg = pgraphics;
+		nodeRadius = pg.height/(float)10;
+		nodeStroke = pg.height/60;
+		circleRadius = pg.height/(float)1.5;
+		circleStroke = pg.height/37;
+		xCenter = pg.width/2;
+		yCenter = pg.height/2;
+		mandalaFont = parent.loadFont("AvantGuard-30.vlw");
+		mandalaHover = new MandalaHover(pg, mandalaFont);
+		mandalaNodeList = new MandalaNode[(int) numberOfNodes];
+		for (int i = 0; i < numberOfNodes; i++) {
+			float x = PApplet.cos(PApplet.radians((float) (i * (360.0 / numberOfNodes))))
+					* (pg.height / 3);
+			float y = PApplet.sin(PApplet.radians((float) (i * (360.0 / numberOfNodes))))
+					* (pg.height / 3);
+			mandalaNodeList[i] = new MandalaNode(pg, i + 1, x, y, nodeRadius, nodeStroke);
+		}
 	}
 	
 
@@ -44,29 +77,34 @@ public class MandalaViewController {
 
 	void displayMandala() {
 
+		pg.beginDraw();
+		pg.smooth();
 		pulser = (float) .006;
 		angle = angle + pulser;
 		pulsate = PApplet.abs(70 * PApplet.sin(angle));
-		parent.ellipseMode(PConstants.CENTER);
-		parent.translate((float)(parent.width/2), (float)(parent.height/2));
+		pg.ellipseMode(PConstants.CENTER);
+		pg.translate(xCenter, yCenter);
 
 		// rot = (mouseX) * .00475;
 		// rotate(rot);
 
-		parent.background(255);
-		parent.stroke(0);
-		parent.strokeWeight(parent.height/37);
-		parent.ellipseMode(PConstants.CENTER);
-		parent.ellipse(0, 0, parent.height/(float)1.5, parent.height/(float)1.5);
-		parent.strokeWeight(parent.height/60);
-		parent.ellipseMode(PConstants.CENTER);
-		parent.ellipse(0, 0, parent.height/(float)10, parent.height/(float)10);
+		pg.background(255);
+		pg.stroke(0);
+		pg.strokeWeight(circleStroke);
+		pg.ellipseMode(PConstants.CENTER);
+		pg.ellipse(0, 0, circleRadius, circleRadius);
+		pg.strokeWeight(nodeStroke);
+		pg.ellipseMode(PConstants.CENTER);
+		pg.ellipse(0, 0, nodeRadius, nodeRadius);
 
 		// textleft();
 
 		drawMandalaNode(mandalaNodeList);
+		mandalaHover.drawNodeHover(parent.mouseX, parent.mouseY, pulsate);
+		pg.endDraw();
+		parent.image(pg,0,0);
 		// hover();
-		System.out.println(button);
+		//System.out.println(button);
 	}
 
 }
---
+++ src/TextColumns.java
@@ -0,0 +1,87 @@
+import processing.core.PApplet;
+
+public class TextColumns {
+
+	private float fallRate;
+	private int charPosX;
+	private int rows;
+	private int x = 0;
+	private int y = 1;
+	private float boundHeight;
+	private float boundWidth;
+	int flag;
+	
+	PApplet parent; // The parent PApplet that we will render ourselves onto
+
+
+	// sets the column position, and column width
+	TextColumns(PApplet p, int posX, float charBounds, float textHght, int rws) {
+		parent = p;
+		if (rws > 0) {
+			rows = rws;
+		} else {
+			rows = 1;
+		}
+
+		if (posX > 0) {
+			charPosX = (posX + 1);
+		} else {
+			charPosX = 1;
+		}
+
+		if (charBounds > 0) {
+			boundWidth = charBounds;
+		} else {
+			boundWidth = 1;
+		}
+
+		if (textHght > 0) {
+			boundHeight = textHght;
+		} else {
+			boundHeight = 1;
+		}
+		fallRate = parent.random(1, 10);
+	}
+
+	void display()
+	{
+		char letter;
+		int let;
+
+		//draws the text column
+		for (int i = 1; i <= y; i++)
+		{
+			let = (int)parent.random(65,88);
+			parent.fill(parent.random(255));
+			letter = (char)let;
+			parent.text(letter, (float)(charPosX*boundWidth-(boundWidth*.25)), i*boundHeight);
+		}
+
+		//sets rate for column growth
+		if(x > fallRate)
+		{
+			if(y >= rows)
+			{
+				y = rows;
+				flag = 1;
+			}
+			else
+			{
+				y++;
+			} 
+			x=0;
+		}
+		x++;
+	}
+	
+	void resetValues(){
+		
+		fallRate = 0;
+		x = 0;
+		y = 1;
+		flag = 0;
+		
+		fallRate = parent.random(1, 10);
+	}
+
+}
