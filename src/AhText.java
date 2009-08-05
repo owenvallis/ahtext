a43ba0c5c51d562ce7dad59f7e50621a020377c0
jhochenbaum <jhochenbaum@localhost>
2009-08-06 00:39:45 +1200

    Adding support for dynamic location translation + added support for dynamic resizing of Mandala

--- src/AhText.java
+++ src/AhText.java
@@ -16,26 +16,22 @@ public class AhText extends PApplet {
 
 	}
 
-
-
 	FullScreen fs;
-
 	PGraphics pg;
-	
-	
+
 	TextViewController textViewController;
 	MandalaViewController mandalaViewController;
 
 
 	public void setup() {
-		
+
 		size(screen.width, screen.height, JAVA2D);
 		pg = createGraphics(screen.width, screen.height, P2D);
-		
+
 		fs = new FullScreen(this);
 		fs.setShortcutsEnabled(false);
 		fs.enter();
-		
+
 		background(255);
 		smooth();
 		noStroke();
@@ -43,20 +39,26 @@ public class AhText extends PApplet {
 		textViewController = new TextViewController(this, pg);
 		mandalaViewController = new MandalaViewController(this);
 
-		
-
 		loop();
-		frameRate(11);
+		frameRate(30);
+		
 	}
+	
+
+	
 
 	public void draw() {
-	if (mousePressed){
-		mandalaViewController.displayMandala();
-	}
-	else{
-		textViewController.displayText();
-	}
-		
+
+		switch(key) {
+
+		case 't':  // text mode
+			textViewController.displayText();  
+			break;
+		case 'm': // mandala mode
+			mandalaViewController.displayMandala();
+			break;
+		}
+
 		
 	}
 }
